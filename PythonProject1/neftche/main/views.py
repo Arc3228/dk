6a52036299100333633f7c95b4e75e57e970588b
Admin <rafael.dronov364@gmail.com>
2025-04-11 21:18:47 +0300

    .

--- PythonProject1/neftche/main/views.py
+++ PythonProject1/neftche/main/views.py
@@ -4,23 +4,23 @@ from .models import Circle, News
 
 def home(request):
     latest_news = News.objects.order_by('-pub_date')[:3]
-    return render(request, 'home.html', {'latest_news': latest_news})
+    return render(request, 'main/home.html', {'latest_news': latest_news})
 
 
 def circles(request):
     circles = Circle.objects.all()
-    return render(request, 'circles.html', {'circles': circles})
+    return render(request, 'main/circles.html', {'circles': circles})
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    return render(request, 'main/contacts.html')
 
 
 def news_list(request):
     news = News.objects.order_by('-pub_date')
-    return render(request, 'news_list.html', {'news_list': news})
+    return render(request, 'main/news_list.html', {'news_list': news})
 
 
 def news_detail(request, news_id):
     item = get_object_or_404(News, id=news_id)
-    return render(request, 'news_detail.html', {'news': item})
+    return render(request, 'main/news_detail.html', {'news': item})
